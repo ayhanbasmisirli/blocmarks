cf9824891ceb050b15133ca2b46b6bc0e3f15af8
Jay Wilburn <jay.wilburn1@gmail.com>
2015-01-12 17:04:50 -0600

    Add Rails

---
+++ blocmarks/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocmarks::Application.config.secret_key_base = 'f49ddb6d827fbcfa3c06afc440507453276f0041258932f81e02091017e1137d66d5451ab7d01280db2cc0b1824e9b4abad2624fd92a480a2f899abc4039eadd'
