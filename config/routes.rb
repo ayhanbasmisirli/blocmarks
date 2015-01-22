396425f5b45b4a7c030a3f41a76481aa19aa327b
Jay Wilburn <jay.wilburn1@gmail.com>
2015-01-22 05:11:21 -0600

    bookmark and topic fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Blocmarks::Application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show, :index]
 
-  resources :topics do 
-    resources :bookmarks, only: [:new, :create]
-  end
-  resources :bookmarks, except: [:new, :create]
+  resources :topics 
+  resources :bookmarks
   
    root to: "welcome#index"
 
